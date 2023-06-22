bdbf16055f12963a838f0fe4e865225bdac14b09
mostafaaziz6 <mostafaaziz.160@gmail.com>
2023-06-21 20:09:48 -0700

    finish task 1

---
+++ lec 1/task 1/mostafa mahmoud/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+    echo("my frist task");
+    echo'<br>';
+    echo'october'.'  november'.'  december';
+     echo '<br>';
+
+     $rusult=563;
+     $difination="fraction";
+     echo$rusult.' '.$difination;
+     echo '<br>';
+     
+    $x=260;
+    $y=&$x;
+    $x=200;
+    echo '<br>';
+    echo$y;
+    echo '<br>';
+
+$h=0;
+if ($h==false){
+    echo "false";
+}else{
+    echo "true";
+};
+echo '<br>';
+ 
+var_dump($h);
+echo '<br>';
+
+echo PHP_INT_MIN;
+echo '<br>';
+
+
+echo floor((0.2+0.1)*0.7);
+echo '<br>';
+    
+    
+    echo "echo";
+    echo '<br>';
+    
+
+    
+    ?>
+    <h1>
+<img src="C:\Users\mosta\Pictures\New folder" alt="difference between echo and print" width="500" height="600"></h1>
+</body>
+</html>
